5996631bb1952e9fb56adb39cbaadd45bac0a95d
preethiramiah <preethiramiah@gmail.com>
2020-05-27 17:13:36 +0530

    scripts file update

--- scripts/deploy-github.js
+++ scripts/deploy-github.js
@@ -1,13 +1,13 @@
-const ghpages = require("gh-pages");
+const ghpages = require('gh-pages')
 
 // replace with your repo url
 ghpages.publish(
-    "public",
-    {
-        branch: "master",
-        repo: "https://github.com/preethiramiah/preethiramiah.github.io.git"
-    },
-    () => {
-        console.log("Deploy Complete!");
-    }
-);
+  'public',
+  {
+    branch: 'master',
+    repo: 'https://github.com/preethiramiah/preethiramiah.github.io.git'
+  },
+  () => {
+    console.log('Deploy Complete!')
+  }
+)
